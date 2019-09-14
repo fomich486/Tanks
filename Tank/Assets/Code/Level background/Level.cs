2f57ef6ce9762cf1b6477510c968d2207bf30924
fomich486 <42343682+fomich486@users.noreply.github.com>
2019-09-14 16:48:24 +0300

    Added play zone field

---
+++ Tank/Assets/Code/Level background/Level.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level
+{
+    [RequireComponent(typeof(Plane))]
+    public class Level : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(4,8)]
+        private int width = 3;
+        [SerializeField]
+        [Range(4, 8)]
+        private int height = 3;
+        // Temp
+        [SerializeField]
+        private GameObject testPlayer;
+
+        private void Awake()
+        {
+            int _width = 2 * width + 1;
+            int _height = 2 * height + 1;
+            Vector2 _levelScale = new Vector2(_width, _height);
+            transform.localScale = (Vector3)_levelScale + Vector3.forward;
+            Renderer _renderer = GetComponent<Renderer>();
+            _renderer.sharedMaterial.mainTextureScale = _levelScale;
+            
+            //Temp
+            Instantiate(testPlayer, Vector3.up * 0.5f, Quaternion.identity);
+        }
+    }
+}
---
+++ Tank/Assets/Code/Player Tank/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Movement : MonoBehaviour
+    {
+        private Vector3 currentDirection;
+
+        public Vector3 CurrentDirection { get => currentDirection; set => currentDirection = value; }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        public void Move()
+        {
+           // transform.position
+        }
+    }
+}
